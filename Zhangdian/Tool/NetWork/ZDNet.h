24209fcd611b80f794f081edd4f7bd6299dd71f8
wenyizhen <731424699@qq.com>
2018-12-05 16:47:42 +0800

    net

---
+++ Zhangdian/Tool/NetWork/ZDNet.h
@@ -0,0 +1,17 @@
+//
+//  ZDNet.h
+//  Zhangdian
+//
+//  Created by W z on 2018/12/5.
+//  Copyright © 2018 wenyizhen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ZDNet : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Zhangdian/Tool/NetWork/ZDNet.m
@@ -0,0 +1,13 @@
+//
+//  ZDNet.m
+//  Zhangdian
+//
+//  Created by W z on 2018/12/5.
+//  Copyright © 2018 wenyizhen. All rights reserved.
+//
+
+#import "ZDNet.h"
+
+@implementation ZDNet
+
+@end
